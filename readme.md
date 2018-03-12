7c6f5c0384bc0609a04609dc87b47b6fec2b17cd
lilyneao <lilyneao@foxmail.com>
2018-03-13 00:38:24 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+React全家桶入门
+http://blog.csdn.net/awaw00/article/details/54693780
+
+npm run server
+npm run dev
+
+第二步，页面未执行 es6语法问题
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    (
+        <div>hello,react</div>
+    ),
+    document.getElementById('app')
+);
